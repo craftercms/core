980f7b4407a036d81a520605ed6baff96da71eae
jdrossl <jdross17@gmail.com>
2017-03-10 14:18:27 -0600

    Refactor for new processors.

---
+++ src/main/java/org/craftercms/core/processors/impl/TaggingByPathProcessor.java
@@ -0,0 +1,61 @@
+package org.craftercms.core.processors.impl;
+
+import org.craftercms.core.processors.ItemProcessor;
+import org.craftercms.core.service.Item;
+
+import java.util.Map;
+
+/**
+ * {@link ItemProcessor} that adds a new tag or field to items on specific paths.
+ * @author joseross
+ */
+public class TaggingByPathProcessor extends AbstractTaggingProcessor {
+
+    /**
+     * Map of paths & values, values can be multiple (separated by commas).
+     */
+    protected Map<String, String> pathMapping;
+
+    public void setPathMapping(Map<String, String> pathMapping) {
+        this.pathMapping = pathMapping;
+    }
+
+    @Override
+    protected String getTagValues(Item item) {
+        String value = null;
+        String path = item.getUrl();
+        for(Map.Entry<String, String> entry : pathMapping.entrySet()) {
+            if (path.matches(entry.getKey())) {
+                value = entry.getValue();
+            }
+        }
+        return value;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TaggingByPathProcessor that = (TaggingByPathProcessor) o;
+
+        if (newField != null ? !newField.equals(that.newField) : that.newField != null) return false;
+        return pathMapping != null ? pathMapping.equals(that.pathMapping) : that.pathMapping == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = newField != null ? newField.hashCode() : 0;
+        result = 31 * result + (pathMapping != null ? pathMapping.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "TaggingByPathProcessor{" +
+            "newField='" + newField + '\'' +
+            ", pathMapping=" + pathMapping +
+            '}';
+    }
+
+}
