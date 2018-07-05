224e402d6c6a9e416360ace721083c5fbebae073
Alfonso Vasquez <alfonso.vasquez@craftersoftware.com>
2018-07-05 17:27:58 -0400

    Item lists are now returned always as JSON arrays:
    * Dom4jDocumentJsonSerializer now returns elements with item-list="true" as JSON arrays.

--- src/main/java/org/craftercms/core/util/json/jackson/Dom4jDocumentJsonSerializer.java
+++ src/main/java/org/craftercms/core/util/json/jackson/Dom4jDocumentJsonSerializer.java
@@ -10,6 +10,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.dom4j.Attribute;
 import org.dom4j.Document;
@@ -39,6 +40,8 @@ import org.dom4j.Node;
  * @author avasquez
  */
 public class Dom4jDocumentJsonSerializer extends JsonSerializer<Document> {
+
+    public static final String ITEM_LIST_ATTRIBUTE_NAME = "item-list";
 
     public static final String TEXT_JSON_KEY = "text";
 
@@ -106,9 +109,11 @@ public class Dom4jDocumentJsonSerializer extends JsonSerializer<Document> {
                 }
             }
 
+            boolean itemList = isItemList(element);
             Map<String, List<Element>> children = getChildren(element);
+
             for (Map.Entry<String, List<Element>> entry : children.entrySet()) {
-                if (entry.getValue().size() > 1) {
+                if (itemList || entry.getValue().size() > 1) {
                     jsonGenerator.writeArrayFieldStart(entry.getKey());
 
                     for (Element child : entry.getValue()) {
@@ -132,7 +137,7 @@ public class Dom4jDocumentJsonSerializer extends JsonSerializer<Document> {
     @SuppressWarnings("unchecked")
     private List<String> getTextContentFromMixedContent(Element element) {
         List<Node> content = element.content();
-        List<String> textContent = new ArrayList<String>();
+        List<String> textContent = new ArrayList<>();
 
         for (Node node : content) {
             if (node.getNodeType() == Node.TEXT_NODE) {
@@ -163,6 +168,10 @@ public class Dom4jDocumentJsonSerializer extends JsonSerializer<Document> {
         }
 
         return groupedChildren;
+    }
+
+    private boolean isItemList(Element element) {
+        return BooleanUtils.toBoolean(element.attributeValue(ITEM_LIST_ATTRIBUTE_NAME));
     }
 
 }
