4bdd9db6b0faf995602824c377e39326f989d0d9
Alfonso Vásquez Drexler <alfonso.vasquez@craftercms.org>
2025-03-31 14:45:50 -0400

    Added support for the application cache (#215)
    * Switch to 5.0.0
    
    * Added support for the application cache
    
    * Fixed javadocs
    
    ---------
    
    Co-authored-by: Sumer Jabri <sumer.jabri@craftercms.org>
    Co-authored-by: Alfonso Vasquez <alfonso.vasquez@craftercms.com>

---
+++ src/main/java/org/craftercms/core/events/ContextEvent.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2007-2025 Crafter Software Corporation. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.craftercms.core.events;
+
+import org.craftercms.core.service.Context;
+import org.springframework.context.ApplicationEvent;
+
+/**
+ * Base class for events related to {@link Context}
+ *
+ * @author avasquez
+ * @since 4.3.1
+ */
+public class ContextEvent extends ApplicationEvent {
+
+    public ContextEvent(Context context) {
+        super(context);
+    }
+
+    public Context getContext() {
+        return (Context) getSource();
+    }
+
+}
